bb067b17a3f76dcd3da35bf13066e0e002e578ea
chaelin1211 <cofls749@naver.com>
2024-03-19 13:46:37 +0900

    STYLE: 상단 배너 레이아웃으로 이동

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-  import "../lib/app.css";
+  import "$lib/app.css";
 </script>
-
-<slot />
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-0">
+    <div class="bg-white rounded-md mt-10 sm:mx-auto sm:w-full sm:max-w-md p-2">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2
+                    class="my-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+            >
+                Simple Web Chat 🤐
+            </h2>
+        </div>
+        <slot/>
+    </div>
+</div>
--- front/src/routes/first/+page.svelte
+++ front/src/routes/first/+page.svelte
@@ -16,40 +16,28 @@
     sessionStorage.setItem("userName", "");
   });
 </script>
-
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="bg-white rounded-md mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2
-        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-      >
-        Simple Web Chat 🤐
-      </h2>
+<form class="p-10 space-y-6" on:submit={(e) => enterChat(e, input)}>
+  <div>
+    <label
+            for="name"
+            class="block text-sm font-medium leading-6 text-gray-900">name</label
+    >
+    <div class="mt-2">
+      <input
+              bind:value={input}
+              id="name"
+              name="name"
+              type="text"
+              required
+              class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
     </div>
-    <form class="p-10 space-y-6" on:submit={(e) => enterChat(e, input)}>
-      <div>
-        <label
-          for="name"
-          class="block text-sm font-medium leading-6 text-gray-900">name</label
-        >
-        <div class="mt-2">
-          <input
-            bind:value={input}
-            id="name"
-            name="name"
-            type="text"
-            required
-            class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-      </div>
-      <div>
-        <button
-          type="submit"
-          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >Enter
-        </button>
-      </div>
-    </form>
   </div>
-</div>
+  <div>
+    <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >Enter
+    </button>
+  </div>
+</form>

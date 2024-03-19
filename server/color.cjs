3c2ae3a211ee3a684c776563017ce46b37b1e9a3
chaelin1211 <cofls749@naver.com>
2024-03-19 15:27:09 +0900

    FEAT: 랜덤 컬러 생성 소스 추가

---
+++ server/color.cjs
@@ -0,0 +1,23 @@
+// Converts integer to hex
+const colToHex = (c) => {
+  // Hack so colors are bright enough
+  let color = c < 75 ? c + 75 : c;
+  let hex = color.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+};
+
+// uses colToHex to concatenate
+// a full 6 digit hex code
+const rgbToHex = (r, g, b) => {
+  return "#" + colToHex(r) + colToHex(g) + colToHex(b);
+};
+
+const getRandomColor = () => {
+  return rgbToHex(
+    Math.floor(Math.random() * 255),
+    Math.floor(Math.random() * 255),
+    Math.floor(Math.random() * 255),
+  );
+};
+
+module.exports = { getRandomColor };

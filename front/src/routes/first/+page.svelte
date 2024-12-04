1720251ebe4a759060c5ee2f84bee04efe30ea79
chaelin1211 <cofls749@naver.com>
2024-12-04 17:56:42 +0900

    FEAT: 코드 별로 방 구분 입장 기능 추가

--- front/src/routes/first/+page.svelte
+++ front/src/routes/first/+page.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import { name, roomCode } from "../../store.js";
+  import { name, openConfirm, openModal, roomCode } from "../../store.js";
   import { onMount } from "svelte";
   import { v4 as uuidv4 } from "uuid";
+  import { PUBLIC_SERVER } from "$env/static/public";
 
-  let input = "";
+  let nickNameInput = "";
+  let roomCodeInput = "";
+  let roomCodeModalShow = false;
 
-  function enterChat(e, input: string, code = "ALL") {
+  async function enterExistedRoom(e, nickNameInput, code = "ALL") {
     e.preventDefault();
-    sessionStorage.setItem("userName", input);
-    name.set(input);
+    if (code !== "ALL") {
+      // 존재하는 Room인지 확인
+      const response = await fetch(
+        PUBLIC_SERVER + `/room-check?roomId=${code}`,
+      );
+      if (response.ok) {
+        const data = await response.json();
+        if (!data.result) {
+          openConfirm("존재하지 않는 방입니다. 새로 만드시겠습니까?", () =>
+            enterRoom(nickNameInput, code),
+          );
+          return;
+        }
+      }
+    }
+
+    enterRoom(nickNameInput, code);
+  }
+
+  function enterRoom(nickNameInput, code = uuidv4()) {
+    if (!checkName(nickNameInput)) return;
+    sessionStorage.setItem("userName", nickNameInput);
+    name.set(nickNameInput);
     roomCode.set(code);
     goto("/chat");
   }
 
-  function createNewRoom(e, input) {
-    enterChat(e, input, uuidv4());
+  function roomInputOpen(nickNameInput) {
+    roomCodeModalShow = checkName(nickNameInput);
+  }
+
+  function checkName(nickNameInput) {
+    if (!nickNameInput) {
+      openModal("이름을 먼저 입력하세요.");
+      return false;
+    }
+    return true;
   }
 
   onMount(() => {
@@ -23,11 +55,14 @@
   });
 </script>
 
-<form class="py-10 space-y-6" on:submit={(e) => enterChat(e, input)}>
+<form
+  class="py-10 space-y-6"
+  on:submit={(e) => enterExistedRoom(e, nickNameInput)}
+>
   <div>
     <div class="mt-2">
       <input
-        bind:value={input}
+        bind:value={nickNameInput}
         id="name"
         name="name"
         type="text"
@@ -49,9 +84,51 @@
     <button
       title="나만의 방 만들기"
       type="button"
-      on:click={(e) => createNewRoom(e, input)}
+      on:click={() => enterRoom(nickNameInput)}
       class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
       >나만의 방 만들기
     </button>
   </div>
+  <div>
+    <button
+      title="기존 방 들어가기"
+      type="button"
+      on:click={() => roomInputOpen(nickNameInput)}
+      class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      >기존 방 들어가기
+    </button>
+  </div>
 </form>
+
+{#if roomCodeModalShow}
+  <div>
+    <div>
+      <div class="mt-2">
+        <input
+          bind:value={roomCodeInput}
+          name="roomCode"
+          type="text"
+          placeholder="name"
+          required
+          class="block w-full rounded-md outline-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
+        />
+      </div>
+      <div>
+        <button
+          title="입장"
+          type="button"
+          on:click={(e) => enterExistedRoom(e, nickNameInput, roomCodeInput)}
+          class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >입장
+        </button>
+        <button
+          title="취소"
+          type="button"
+          on:click={() => (roomCodeModalShow = false)}
+          class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >취소
+        </button>
+      </div>
+    </div>
+  </div>
+{/if}
